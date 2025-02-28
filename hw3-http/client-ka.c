a2a8ca9e9348d1755cc5bd666c85e6390b7adc7b
Zachary Hopp <zacharyhopp01@gmail.com>
2025-02-28 15:22:40 -0500

    Part 2 HW3

--- hw3-http/client-ka.c
+++ hw3-http/client-ka.c
@@ -6,36 +6,38 @@
 #include <unistd.h>
 #include <string.h>
 
-#define  BUFF_SZ            1024
-#define  MAX_REOPEN_TRIES   5
+#define BUFF_SZ 1024
+#define MAX_REOPEN_TRIES 5
 
 char recv_buff[BUFF_SZ];
 
-char *generate_cc_request(const char *host, int port, const char *path){
-	static char req[512] = {0};
-	int offset = 0;
-	
-    //note that all paths should start with "/" when passed in
-	offset += sprintf((char *)(req + offset),"GET %s HTTP/1.1\r\n", path);
-	offset += sprintf((char *)(req + offset),"Host: %s\r\n", host);
-	offset += sprintf((char *)(req + offset),"Connection: Keep-Alive\r\n");
-	offset += sprintf((char *)(req + offset),"\r\n");
+char *generate_cc_request(const char *host, int port, const char *path)
+{
+    static char req[512] = {0};
+    int offset = 0;
 
-	printf("DEBUG: %s", req);
-	return req;
+    // note that all paths should start with "/" when passed in
+    offset += sprintf((char *)(req + offset), "GET %s HTTP/1.1\r\n", path);
+    offset += sprintf((char *)(req + offset), "Host: %s\r\n", host);
+    offset += sprintf((char *)(req + offset), "Connection: Keep-Alive\r\n");
+    offset += sprintf((char *)(req + offset), "\r\n");
+
+    printf("DEBUG: %s", req);
+    return req;
 }
 
-
-void print_usage(char *exe_name){
+void print_usage(char *exe_name)
+{
     fprintf(stderr, "Usage: %s <hostname> <port> <path...>\n", exe_name);
-    fprintf(stderr, "Using default host %s, port %d  and path [\\]\n", DEFAULT_HOST, DEFAULT_PORT); 
+    fprintf(stderr, "Using default host %s, port %d  and path [\\]\n", DEFAULT_HOST, DEFAULT_PORT);
 }
 
-int reopen_socket(const char *host, uint16_t port) {
+int reopen_socket(const char *host, uint16_t port)
+{
     int sock = 0;
 
     //----------------------------------------------------------------------------
-    //TODO: Implement a loop that attempts a certain number of times to open a 
+    // TODO: Implement a loop that attempts a certain number of times to open a
     //      socket with the host and port that is passed in as parameters.
     //
     //      I created a constant called MAX_REOPEN_TRIES (currently set to 5)
@@ -44,8 +46,8 @@ int reopen_socket(const char *host, uint16_t port) {
     //
     //      What you need to do:
     //          1. Create a loop that loops MAX_REOPEN_TRIES
-    //          2. In the body of the loop attempt to connect to the server.  
-    //             use the sock = socket_connect(host,port)  function to 
+    //          2. In the body of the loop attempt to connect to the server.
+    //             use the sock = socket_connect(host,port)  function to
     //             do this.
     //          3. The socket_connect function will return a negative number if
     //             the socket_connect() function fails.  Continue looping if this
@@ -53,82 +55,98 @@ int reopen_socket(const char *host, uint16_t port) {
     //          4. If socket_connect() returns a positive number it is a valid
     //             socket so just return it, e.g., return sock
     //          5. If we fall out of the loop, we are unable to connect, return
-    //             -1 to indicate a failure. 
+    //             -1 to indicate a failure.
     //----------------------------------------------------------------------------
+    for (int try = 0; try < MAX_REOPEN_TRIES; try++)
+    {
+        sock = socket_connect(host, port);
+        if (sock >= 0)
+        {
+            return sock;
+        }
+    }
 
-    
     return -1;
 }
 
-int server_connect(const char *host, uint16_t port){
+int server_connect(const char *host, uint16_t port)
+{
     return socket_connect(host, port);
 }
 
-void server_disconnect(int sock){
+void server_disconnect(int sock)
+{
     close(sock);
 }
 
-int submit_request(int sock, const char *host, uint16_t port, char *resource){
-    int sent_bytes = 0; 
+int submit_request(int sock, const char *host, uint16_t port, char *resource)
+{
+    int sent_bytes = 0;
 
     const char *req = generate_cc_request(host, port, resource);
     int send_sz = strlen(req);
 
-    // This is the initial send, this is where the send will fail with 
+    // This is the initial send, this is where the send will fail with
     // Keep-Alive if the server closed the socket, sent_bytes will have
     // the number of bytes sent, which should equal send_sz if all went
     // well.  If sent_bytes < 0, this indicates a send error, so we will
     // need to reconnect to the server.
-    sent_bytes = send(sock, req, send_sz,0);
+    sent_bytes = send(sock, req, send_sz, 0);
 
-    //we have a socket error, perhaps the server closed it, lets try to reopen
-    //the socket
-    if (sent_bytes < 0){
+    // we have a socket error, perhaps the server closed it, lets try to reopen
+    // the socket
+    if (sent_bytes < 0)
+    {
         //----------------------------------------------------------------------------
-        //TODO:  Reimplement the retry logic to reopen the socket
+        // TODO:  Reimplement the retry logic to reopen the socket
         //
         // The variable sock needs to be reset to a new socket with the server.
         // 1.  Use the sock = reopen_socket() helper you implemented above in an attempt
-        //     to reopen_the socket.  If it returns a value less than zero, a socket 
+        //     to reopen_the socket.  If it returns a value less than zero, a socket
         //     could not be created so return the negative value and exit the function,
         //     e.g., return sock
         //
         //  2. Assuming you got a valid socket, reissue the send again
         //     sent_bytes = send(sock, req, send_sz,0);
         //----------------------------------------------------------------------------
-        
-        return -1;  //remove this line of code, i just want this to compile so the
-                    //block of code needs at least one line
+        sock = reopen_socket(host, port);
+        if (sock < 0)
+        {
+            return sock;
+        }
+        sent_bytes = send(sock, req, send_sz, 0);
     }
 
-    //This should not happen, but just checking if we didnt send everything and 
-    //handling appropriately 
-    if(sent_bytes != send_sz){
-        if(sent_bytes < 0)
+    // This should not happen, but just checking if we didnt send everything and
+    // handling appropriately
+    if (sent_bytes != send_sz)
+    {
+        if (sent_bytes < 0)
             perror("send failed after reconnect attempt");
         else
             fprintf(stderr, "Sent bytes %d is not equal to sent size %d\n", sent_bytes, send_sz);
-        
+
         close(sock);
         return -1;
     }
 
-    int bytes_recvd = 0;    //used to track amount of data received on each recv() call
-    int total_bytes = 0;    //used to accumulate the total number of bytes across all recv() calls
-    
-    //do the first recv
-    bytes_recvd = recv(sock, recv_buff, sizeof(recv_buff),0);
-    if(bytes_recvd < 0) {
+    int bytes_recvd = 0; // used to track amount of data received on each recv() call
+    int total_bytes = 0; // used to accumulate the total number of bytes across all recv() calls
+
+    // do the first recv
+    bytes_recvd = recv(sock, recv_buff, sizeof(recv_buff), 0);
+    if (bytes_recvd < 0)
+    {
         perror("initial receive failed");
         close(sock);
         return -1;
     }
 
-    //remember the first receive we just did has the HTTP header, and likely some body
-    //data.  We need to determine how much data we expect
+    // remember the first receive we just did has the HTTP header, and likely some body
+    // data.  We need to determine how much data we expect
 
     //--------------------------------------------------------------------------------
-    //TODO:  Get the header len
+    // TODO:  Get the header len
     //
     // 1. Use the get_http_header_len() function to set the header_len variable.
     // 2. The get_http_header_len() function returns a negative value if it fails, so
@@ -136,11 +154,14 @@ int submit_request(int sock, const char *host, uint16_t port, char *resource){
     //          a. close the socket -- close(sock)
     //          b. return -1 to exit this function
     //--------------------------------------------------------------------------------
-    int header_len = 0;     //change this to get the header len as per the directions above
-    
-
+    int header_len = get_http_header_len(recv_buff, bytes_recvd); // change this to get the header len as per the directions above
+    if (header_len < 0)
+    {
+        close(sock);
+        return -1;
+    }
     //--------------------------------------------------------------------------------
-    //TODO:  Get the conetent len
+    // TODO:  Get the conetent len
     //
     // 1. Use the get_http_content_len() function to set the content_len variable.
     //
@@ -148,26 +169,26 @@ int submit_request(int sock, const char *host, uint16_t port, char *resource){
     // cannot find a Content-Length header, its assumed as per the HTTP spec that ther
     // is no body, AKA, content_len is zero;
     //--------------------------------------------------------------------------------
-    int content_len = 0;    //Change this to get the content length
+    int content_len = get_http_content_len(recv_buff, bytes_recvd); // Change this to get the content length
 
     //--------------------------------------------------------------------------------
     // TODO:  Make sure you understand the calculations below
     //
-    // You do not have to write any code, but add to this comment your thoughts on 
+    // You do not have to write any code, but add to this comment your thoughts on
     // what the following 2 lines of code do to track the amount of data received
     // from the server
     //
-    // YOUR ANSWER:  <START-YOUR-RESPONSE-HERE>
+    // YOUR ANSWER:  Removing the length of the header tells us the exact of amount of data we received at a particular time, removing that amount of particular data from content_len allows us to know how much data we still need to receive to have the full content.
     //
     //--------------------------------------------------------------------------------
-    int initial_data =  bytes_recvd - header_len;
+    int initial_data = bytes_recvd - header_len;
     int bytes_remaining = content_len - initial_data;
 
-
-    //This loop keeps going until bytes_remaining is essentially zero, to be more
-    //defensive an prevent an infinite loop, i have it set to keep looping as long
-    //as bytes_remaining is positive
-    while(bytes_remaining > 0){
+    // This loop keeps going until bytes_remaining is essentially zero, to be more
+    // defensive an prevent an infinite loop, i have it set to keep looping as long
+    // as bytes_remaining is positive
+    while (bytes_remaining > 0)
+    {
         //-----------------------------------------------------------------------------
         // TODO:  Continue receiving data from the server
         //
@@ -177,21 +198,26 @@ int submit_request(int sock, const char *host, uint16_t port, char *resource){
         //      a. close the socket (sock)
         //      b. return -1 to indicate an error
         //-----------------------------------------------------------------------------
-        bytes_recvd = 0; // replace with a valid recv(...); call
-        
-        //You can uncomment out the fprintf() calls below to see what is going on
+        bytes_recvd = recv(sock, recv_buff + total_bytes, bytes_remaining, 0); // replace with a valid recv(...); call
 
-        //fprintf(stdout, "%.*s", bytes_recvd, recv_buff);
+        if (bytes_recvd < 0)
+        {
+            close(socket);
+            return -1;
+        }
+        // You can uncomment out the fprintf() calls below to see what is going on
+
+        // fprintf(stdout, "%.*s", bytes_recvd, recv_buff);
         total_bytes += bytes_recvd;
-        //fprintf(stdout, "remaining %d, received %d\n", bytes_remaining, bytes_recvd);
+        // fprintf(stdout, "remaining %d, received %d\n", bytes_remaining, bytes_recvd);
         bytes_remaining -= bytes_recvd;
     }
 
     fprintf(stdout, "\n\nOK\n");
     fprintf(stdout, "TOTAL BYTES: %d\n", total_bytes);
 
-    //processed the request OK, return the socket, in case we had to reopen
-    //so that it can be used in the next request
+    // processed the request OK, return the socket, in case we had to reopen
+    // so that it can be used in the next request
 
     //---------------------------------------------------------------------------------
     // TODO:  Documentation
@@ -199,40 +225,46 @@ int submit_request(int sock, const char *host, uint16_t port, char *resource){
     // You dont have any code to change, but explain why this function, if it gets to this
     // point returns an active socket.
     //
-    // YOUR ANSWER:  <START-YOUR-RESPONSE-HERE>
+    // YOUR ANSWER:  This function will return the active socket at this point because it did not encounter any errors in reopening the socket, sending our data, receiving initial data, verifing the HTTP header,  and finally if there are no errors receiving the rest of the data.
     //
     //--------------------------------------------------------------------------------
     return sock;
 }
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
     int sock;
 
     const char *host = DEFAULT_HOST;
-    uint16_t   port = DEFAULT_PORT;
-    char       *resource = DEFAULT_PATH;
-    int        remaining_args = 0;
+    uint16_t port = DEFAULT_PORT;
+    char *resource = DEFAULT_PATH;
+    int remaining_args = 0;
 
-    //YOU DONT NEED TO DO ANYTHING OR MODIFY ANYTHING IN MAIN().  MAKE SURE YOU UNDERSTAND
-    //THE CODE HOWEVER
+    // YOU DONT NEED TO DO ANYTHING OR MODIFY ANYTHING IN MAIN().  MAKE SURE YOU UNDERSTAND
+    // THE CODE HOWEVER
     sock = server_connect(host, port);
 
-    if(argc < 4){
+    if (argc < 4)
+    {
         print_usage(argv[0]);
-        //process the default request
+        // process the default request
         submit_request(sock, host, port, resource);
-	} else {
+    }
+    else
+    {
         host = argv[1];
         port = atoi(argv[2]);
         resource = argv[3];
-        if (port == 0) {
+        if (port == 0)
+        {
             fprintf(stderr, "NOTE: <port> must be an integer, using default port %d\n", DEFAULT_PORT);
             port = DEFAULT_PORT;
         }
         fprintf(stdout, "Running with host = %s, port = %d\n", host, port);
-        remaining_args = argc-3;
-        for(int i = 0; i < remaining_args; i++){
-            resource = argv[3+i];
+        remaining_args = argc - 3;
+        for (int i = 0; i < remaining_args; i++)
+        {
+            resource = argv[3 + i];
             fprintf(stdout, "\n\nProcessing request for %s\n\n", resource);
             sock = submit_request(sock, host, port, resource);
         }
